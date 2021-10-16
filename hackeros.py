9e9924871832e31d48be3fb4dc197afa48a2289d
malxcolm <89610579+malxcolm@users.noreply.github.com>
2021-10-16 12:32:29 +0200

    Create hackeros.py

---
+++ hackeros.py
@@ -0,0 +1,12 @@
+import time
+import os 
+from tqdm import tqdm 
+print("Loading things up")
+time.sleep(2)
+print("Loading sOS.py")
+for i in tqdm (range (101),
+			desc="Loading bOS.py",
+			ascii=False, ncols=75):
+	time.sleep(24)
+	
+print("Complete.")
